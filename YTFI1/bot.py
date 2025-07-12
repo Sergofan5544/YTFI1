61fc301d81cc7013c3a1b504166d5bd3c3a1458d
Sergofan5544 <rsm42@list.ru>
2025-07-12 23:52:20 +0700

    Update bot.py

--- YTFI1/bot.py
+++ YTFI1/bot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(7546967677:AAFIuOROtRfs9MH-WUn9Y7NJcrZGE0Iekoo)
 @bot.message_handler(commands=['start'])
 def start(message):
     keyboard = telebot.types.InlineKeyboardMarkup()
-    youtube_button = telebot.types.InlineKeyboardButton(text="YouTube", web_app=telebot.types.WebAppInfo(url="URL_TO_YOUR_WEBAPP"))
+    youtube_button = telebot.types.InlineKeyboardButton(text="YouTube", web_app=telebot.types.WebAppInfo(url="https://sergofan5544.github.io/YTFI1/"))
     keyboard.add(youtube_button)
     bot.send_message(message.chat.id, "Нажмите кнопку для открытия YouTube:", reply_markup=keyboard)
 
