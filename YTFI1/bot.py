d7d8445dffd871c647c34d1d7a5059192e9507b4
Sergofan5544 <rsm42@list.ru>
2025-07-12 23:32:27 +0700

    Add files via upload

---
+++ YTFI1/bot.py
@@ -0,0 +1,15 @@
+import telebot
+
+# Подставьте сюда токен Вашего бота
+TOKEN = 'YOUR_BOT_TOKEN'
+bot = telebot.TeleBot(7546967677:AAFIuOROtRfs9MH-WUn9Y7NJcrZGE0Iekoo)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    youtube_button = telebot.types.InlineKeyboardButton(text="YouTube", web_app=telebot.types.WebAppInfo(url="URL_TO_YOUR_WEBAPP"))
+    keyboard.add(youtube_button)
+    bot.send_message(message.chat.id, "Нажмите кнопку для открытия YouTube:", reply_markup=keyboard)
+
+if __name__ == "__main__":
+    bot.polling()
